16079b5f76ee749776a1ab579a337c500adbafbd
svnoliveira <srkuran@gmail.com>
2024-02-29 01:05:20 -0300

    style: adjusted text style to center in mobile car name

--- src/components/ReservationInfo/index.tsx
+++ src/components/ReservationInfo/index.tsx
@@ -13,7 +13,7 @@ export const ReservationInfo = ({
 }: IReservationInfoProps) => {
   return (
     <div className="flex flex-col justify-between items-center md:table-cell md:align-middle md:border-y md:border-gray-200 md:pl-4 md:py-1">
-      <span className="block text-base font-semibold text-gray-900 md:hidden">{name}</span>
+      <span className="block text-base font-semibold text-gray-900 text-center md:hidden">{name}</span>
       <div className="flex gap-1 text-xs font-normal text-gray-500 md:text-sm">
         <span className="block md:hidden">Next reservation: </span>
         <span>{`${
